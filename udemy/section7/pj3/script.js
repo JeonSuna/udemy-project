65d77cc8a24414803effff1ac32c662c830c7706
JeonSuna <jsa020910@gmail.com>
2024-09-25 23:41:22 +0900

    첫 화면 구성

---
+++ udemy/section7/pj3/script.js
@@ -0,0 +1,9 @@
+'use strict';
+let score0EL = document.querySelector('#score--0')
+let score1EL = document.querySelector('#score--1')
+
+score0EL.textContent=0
+score1EL.textContent = 0
+
+let dice = document.querySelector('.dice')
+dice.classList.add('hidden')
