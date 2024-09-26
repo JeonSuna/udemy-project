dde539b5b80e6cdf865423041d37acc43c1a609d
JeonSuna <jsa020910@gmail.com>
2024-09-26 17:50:21 +0900

    피그게임 중간 완성 (우승자 출력 전)

--- udemy/section7/pj3/script.js
+++ udemy/section7/pj3/script.js
@@ -7,6 +7,8 @@ let currentScore2 = document.querySelector('#current--1')
 let player0El=document.querySelector('.player--0')
 let player1El=document.querySelector('.player--1')
     
+//index[0]=>player1의 점수 ,
+const score=[0,0]
 score0EL.textContent=0
 score1EL.textContent = 0
 
@@ -15,6 +17,15 @@ dice.classList.add('hidden')
 let rollDice = document.querySelector('.btn--roll')
 let currentScore = 0
 let activePlayer=0
+
+const switchFun = function () {
+    document.querySelector(`#current--${activePlayer}`).textContent = 0
+    currentScore = 0
+    activePlayer = activePlayer === 0 ? 1 : 0
+    player0El.classList.toggle('player--active')    //토글: 클래스 있나 없나 확인 //있으면 제거, 없으면 추가
+    player1El.classList.toggle('player--active')
+}
+
 
 rollDice.addEventListener('click', (e) => {
    
@@ -27,19 +38,19 @@ rollDice.addEventListener('click', (e) => {
         currentScore+=randomDice
         document.querySelector(`#current--${activePlayer}`).textContent = currentScore
     } else {
-        document.querySelector(`#current--${activePlayer}`).textContent = 0
-        currentScore=0
-        activePlayer = activePlayer === 0 ? 1 : 0
-        player0El.classList.toggle('player--active') 
-        player1El.classList.toggle('player--active')
-        //토글: 클래스 있나 없나 확인 //있으면 제거, 없으면 추가
+       
+       switchFun()
     }
 })
 
 let btnHold = document.querySelector('.btn--hold')
 btnHold.addEventListener('click', () => {
-    document.querySelector(`#score--${activePlayer}`).textContent = currentScore
-    currentScore1.textContent = 0
-    currentScore2.textContent=0
-    //스위치 
+    console.log(score[activePlayer])
+    score[activePlayer]+=currentScore
+    document.querySelector(`#score--${activePlayer}`).textContent = score[activePlayer]
+    // currentScore1.textContent = 0
+    // currentScore2.textContent=0
+    // //스위치 
+    switchFun()
 })
+
