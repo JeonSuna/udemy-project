19431417434e6000aebc47ee8c72f31983f985c9
JeonSuna <jsa020910@gmail.com>
2024-11-30 20:33:01 +0900

    destructing-object

--- pratice-class/09/Destructing/object/main.js
+++ pratice-class/09/Destructing/object/main.js
@@ -12,9 +12,6 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  order: function (starterIndex, MainIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[MainIndex]];
-  },
   openingHours: {
     thu: {
       open: 12,
@@ -29,4 +26,47 @@ const restaurant = {
       close: 24,
     },
   },
+  order: function (starterIndex, MainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[MainIndex]];
+  },
+  orderDelivery: function (obj) {
+    console.log(obj); // 밑에 그대로 출력
+  },
 };
+
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del Sole 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+//객체는 배열과 다르게 순서에 의존하지 않고 key값으로 뽑아낸다
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories); //Classico Italiano ,,,등등
+//하지만 변수 이름이 속성이름과 다르길 원한다면?
+const {
+  name: restaurantName, //새로운 변수명
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags); //위 결과와 같음
+
+const { menu = [], starterMenu: starters } = restaurant; //[]로 기본값을 설정한다.undefined를 막기 위해
+
+console.log(menu, starters); //[] ,  ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad']
+//menu처럼 하드코딩 된 데이터가 없을때는 기본값 설정하는게 좋다
+
+//변수 변형
+let a = 111;
+let b = 999;
+const obj = { a: 28, b: 6, c: 14 };
+//  { a, b } = obj; // Unexpected token '=' error
+({ a, b } = obj); //==>괄호로 감싸야함
+console.log(a, b); //=>28,6
+
+//중텁된 객체
+const {
+  fri: { open, close },
+} = openingHours;
+console.log(open, close); //11,23
